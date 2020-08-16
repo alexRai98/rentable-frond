17bace1dd3a9afd5a0e68f3a8caea420b01afeaa
alexRai98 <raikcuerda19@gmail.com>
2020-08-16 10:57:55 -0500

    layput

--- src/features/layout/layout.js
+++ src/features/layout/layout.js
@@ -1,17 +1,45 @@
-import React, { Fragment } from 'react';
-import Navbar from './navBar';
-import Footer from './footer';
+import React, { Fragment } from "react";
+import Navbar from "./navBar";
+import Footer from "./footer";
 
-function Layout({children}){
+function Layout({ children }) {
+  const fetchPhoto = async (photo) => {
+    const response = await fetch("http://localhost:3000/properties", {
+      method: "POST",
+      body: JSON.stringify({images: [photo], addres: "tu casa"}),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok){
+        console.log("exito")
+    }else{
+        console.log("error")
+    }
+  };
 
-    return(
-        <Fragment>
-            <Navbar/>
-            {children}
-            <Footer/>
-        </Fragment>
+  const submitPhoto =async (event) => {
+    event.preventDefault();
+    const photoInput = document.getElementById("images-property");
+    console.log(photoInput.files[0]);
+    await fetchPhoto(photoInput.files[0]);
 
-    )
+  };
+  return (
+    <Fragment>
+      <Navbar />
+      {children}
+      <form onSubmit={submitPhoto}>
+        <input
+          type="file"
+          id="images-property"
+          name="images"
+          accept="image/png, image/jpeg"
+        />
+        <input className="submit-input" type="submit" value="Upload" />
+      </form>
+
+      <Footer />
+    </Fragment>
+  );
 }
 
 export default Layout;
